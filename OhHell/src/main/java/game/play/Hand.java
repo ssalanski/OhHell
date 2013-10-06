9152dd39eb0bd8049551c1fb20e9d04f1f4d3874
ssalanski <stefan.salanski@gmail.com>
2013-10-05 20:46:09 -0400

    Hand can filter legal plays based on led suit

--- OhHell/src/main/java/game/play/Hand.java
+++ OhHell/src/main/java/game/play/Hand.java
@@ -1,6 +1,7 @@
 package game.play;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import utils.misc.StringUtil;
 
@@ -9,6 +10,30 @@ public class Hand extends ArrayList<Card>{
 	public String toString()
 	{
 		return "Hand: " + StringUtil.join(", ", this);
+	}
+	
+	public List<Card> legalPlays(Suit lead)
+	{
+		if (lead == null) // then you are in the lead, all cards legal.
+		{
+			return this;
+		}
+		ArrayList<Card> legal = new ArrayList<Card>();
+		for (Card card : this)
+		{
+			if (card.getSuit().equals(lead))
+			{
+				legal.add(card);
+			}
+		}
+		if (legal.size()>0) // then you the suit that was led, must follow suit
+		{
+			return legal;
+		}
+		else // then you are void in the suit led, so play whatever.
+		{
+			return this;
+		}
 	}
 	
 }
