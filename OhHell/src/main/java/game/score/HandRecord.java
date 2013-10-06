50b554fc592d25395afc562f8b637d55ec5302fa
ssalanski <stefan.salanski@gmail.com>
2013-10-05 20:39:36 -0400

    HandRecord now a map Player->Status, plus other fields

--- OhHell/src/main/java/game/score/HandRecord.java
+++ OhHell/src/main/java/game/score/HandRecord.java
@@ -1,20 +1,44 @@
 package game.score;
 
-import java.util.ArrayList;
+import game.play.Player;
+import game.play.Suit;
+
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class HandRecord {
 
-	private List<Status> playerStats;
+	private Map<Player,Status> playerStats;
+	private Suit trump;
 	
-	public HandRecord(int numPlayers)
+	public HandRecord(List<Player> players)
 	{
-		playerStats = new ArrayList<Status>(numPlayers);
+		playerStats = new HashMap<Player,Status>(players.size());
+		for (Player player : players)
+		{
+			playerStats.put(player,new Status());
+		}
 	}
 	
 	public Status getStatusOf(int player)
 	{
 		return playerStats.get(player);
+	}
+
+	public Suit getTrump() {
+		return trump;
+	}
+
+	public void setTrump(Suit trump) {
+		this.trump = trump;
+	}
+
+	public void assessScores() {
+		for(Status s : playerStats.values())
+		{
+			s.calculateScore();
+		}
 	}
 	
 }
