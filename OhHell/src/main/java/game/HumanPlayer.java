b347ab828a94dad7f8ffe7c658c35acaeb8b50d8
ssalanski <stefan.salanski@gmail.com>
2013-10-10 00:05:41 -0400

    added bidding logic to human player

--- OhHell/src/main/java/game/HumanPlayer.java
+++ OhHell/src/main/java/game/HumanPlayer.java
@@ -1,5 +1,6 @@
 package game;
 
+import utils.ui.UserUtil;
 import game.play.Board;
 import game.play.Card;
 import game.play.Player;
@@ -12,8 +13,37 @@ public class HumanPlayer extends Player {
 
 	@Override
 	public int bid(int tricksThisHand, int tricksRemaining, boolean restricted) {
-		// TODO Auto-generated method stub
-		return 0;
+		System.out.println("There are " + tricksThisHand + " tricks to take this hand, " + tricksRemaining + " have already been bid on.");
+		if(restricted)
+		{
+			System.out.println("You are the dealer, and therefore cannot bid " + tricksRemaining);
+		}
+		Integer bidRequest = null;
+		do
+		{
+			bidRequest = Integer.parseInt(UserUtil.ask("How many would you like to bid?"));
+		} while ( notAcceptable(bidRequest, tricksThisHand, tricksRemaining, restricted));
+		return bidRequest;
+	}
+
+	private boolean notAcceptable(Integer bidRequest, int tricksThisHand, int tricksRemaining, boolean restricted) {
+		if (bidRequest == null)
+		{
+			return false;
+		}
+		if (bidRequest < 0) // dont bid negative
+		{
+			return false;
+		}
+		if (bidRequest > tricksThisHand) // dont bid more than the total number of tricks
+		{
+			return false;
+		}
+		if (restricted && bidRequest==tricksRemaining) // you're the dealer, cant make total bid = number of tricks.
+		{
+			return false;
+		}
+		return true;
 	}
 
 	@Override
