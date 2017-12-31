d3b44d7467c9c04f215e14e42ff2adc84acfa05c
ssalanski <stefan.salanski@gmail.com>
2017-12-30 21:55:17 -0500

    add a CardModel c# script, with some public Sprite fields so we can drag and drop our card sprites in

---
+++ Assets/Scripts/CardModel.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class CardModel : MonoBehaviour
+{
+
+    public Sprite[] clubs_faces;
+    public Sprite[] diamonds_faces;
+    public Sprite[] hearts_faces;
+    public Sprite[] spades_faces;
+    public Sprite cardback;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
