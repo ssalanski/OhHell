2caac263f2f0606d4f73d2e05f4eb44d509155a1
ssalanski <stefan.salanski@gmail.com>
2018-01-14 17:19:03 -0500

    add Hand object as child of the Table, and a HandModel script with a List of CardModel objects

---
+++ Assets/Scripts/HandModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class HandModel : MonoBehaviour
+{
+    public List<CardModel> cards;
+    public GameObject cardPrefab;
+
+    // Use this for initialization
+    void Start()
+    {
+        cards = new List<CardModel>();
+    }
+}
