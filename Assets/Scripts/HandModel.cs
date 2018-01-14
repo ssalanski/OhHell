b133ddb409cbed51ecdeb5866f58212c02c9815f
ssalanski <stefan.salanski@gmail.com>
2018-01-14 17:29:29 -0500

    assign the card prefab to the field in HandModel script, as a test, make the hand instantiate a card

--- Assets/Scripts/HandModel.cs
+++ Assets/Scripts/HandModel.cs
@@ -10,5 +10,14 @@ public class HandModel : MonoBehaviour
     void Start()
     {
         cards = new List<CardModel>();
+
+        GameObject newCard = Instantiate(cardPrefab,gameObject.transform);
+        newCard.GetComponent<SpriteRenderer>().sortingLayerName = "Hand";
+        CardModel cm = newCard.GetComponent<CardModel>();
+        cm.showing = true;
+        cm.SetCard(new Card(Suit.Diamonds, 8));
+
+        cards.Add(cm);
+
     }
 }
