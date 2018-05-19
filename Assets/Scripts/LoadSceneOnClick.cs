ee28ccc87ced645d149d092beacc8fdc6c686472
ssalanski <stefan.salanski@gmail.com>
2018-05-19 18:29:01 -0400

    add functionality to start button to load scene 1
    will create scene 1 later

---
+++ Assets/Scripts/LoadSceneOnClick.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneOnClick : MonoBehaviour {
+
+	public void LoadByIndex(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+}
