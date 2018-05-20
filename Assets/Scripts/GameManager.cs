d81cd446820696eb97232e5c6010d462da452a3e
ssalanski <stefan.salanski@gmail.com>
2018-05-19 23:17:26 -0400

    add GameManager script, attached to the table gameobject
    currently just instantiates a player hand and deals it aces when the spacebar is pressed

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    HandModel playerHand;
+    public GameObject handAnchorPrefab;
+
+    void Start()
+    {
+        GameObject handAnchor;
+        handAnchor = Instantiate(handAnchorPrefab, gameObject.transform);
+        playerHand = handAnchor.GetComponent<HandModel>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            playerHand.TakeCard(new Card(Suit.Diamonds, 1));
+        }
+    }
+
+}
