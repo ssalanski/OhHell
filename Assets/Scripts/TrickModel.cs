7c48366401b3fccf1d0d3d46c1c00c6136440fc2
ssalanski <stefan.salanski@gmail.com>
2018-05-23 23:52:47 -0400

    have first card played in trick set the lead suit

--- Assets/Scripts/TrickModel.cs
+++ Assets/Scripts/TrickModel.cs
@@ -7,6 +7,7 @@ public class TrickModel : MonoBehaviour
     
     Dictionary<HandModel, CardModel> cards;
     public GameObject cardPrefab;
+    public Suit? lead; // is using nullable '?' the best practice?
 
     // Use this for initialization
     void Start()
@@ -26,6 +27,12 @@ public class TrickModel : MonoBehaviour
         cardModel.transform.parent = gameObject.transform;
         cardModel.transform.localPosition = new Vector3(0, 0, 0);
         cardModel.transform.Translate(Vector3.down, sourceHand);
+
+        if(lead==null)
+        {
+            lead = playedCard.GetComponent<CardModel>().thisCard.suit;
+        }
+
     }
 
     // Update is called once per frame
