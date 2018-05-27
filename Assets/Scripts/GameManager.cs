15088604aed2b1c6c332e623928bc11bd3f0c83a
ssalanski <stefan.salanski@gmail.com>
2018-05-26 21:12:39 -0400

    change placement of hand anchors to be clockwise around the table (to make turn order easier)

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -47,12 +47,12 @@ public class GameManager : MonoBehaviour
         {
             handAnchor = Instantiate(handAnchorPrefab, gameObject.transform);
             handAnchor.transform.localPosition += getEllipsePositionAtAngle(30);
-            handAnchor.transform.Rotate(new Vector3(0, 0, 120));
+            handAnchor.transform.Rotate(new Vector3(0, 0, -120));
             allHands.Add(handAnchor.GetComponent<HandModel>());
 
             handAnchor = Instantiate(handAnchorPrefab, gameObject.transform);
             handAnchor.transform.localPosition += getEllipsePositionAtAngle(150);
-            handAnchor.transform.Rotate(new Vector3(0, 0, -120));
+            handAnchor.transform.Rotate(new Vector3(0, 0, 120));
             allHands.Add(handAnchor.GetComponent<HandModel>());
         }
         else
@@ -63,7 +63,7 @@ public class GameManager : MonoBehaviour
                 float playerAngle = playerSpacing * playerIdx;
                 handAnchor = Instantiate(handAnchorPrefab, gameObject.transform);
                 handAnchor.transform.localPosition += getEllipsePositionAtAngle(playerAngle);
-                handAnchor.transform.Rotate(new Vector3(0, 0, 90 + playerAngle));
+                handAnchor.transform.Rotate(new Vector3(0, 0, 270 - playerAngle));
                 allHands.Add(handAnchor.GetComponent<HandModel>());
             }
         }
@@ -109,7 +109,7 @@ public class GameManager : MonoBehaviour
     private Vector3 getEllipsePositionAtAngle(float angle)
     {
         float theta = angle * Mathf.Deg2Rad;
-        float x = tableMajor * tableMinor / Mathf.Sqrt(tableMinor * tableMinor + Mathf.Pow(tableMajor * Mathf.Tan(theta), 2)) * Mathf.Sign(Mathf.Cos(theta));
+        float x = tableMajor * tableMinor / Mathf.Sqrt(tableMinor * tableMinor + Mathf.Pow(tableMajor * Mathf.Tan(theta), 2)) * Mathf.Sign(-Mathf.Cos(theta));
         float y = tableMinor * Mathf.Sqrt(1 - Mathf.Pow(x / tableMajor, 2));
         return new Vector3(x, y, 0);
     }
