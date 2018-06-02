05fb83fefef6a62afd35a2176776b22e483182be
ssalanski <stefan.salanski@gmail.com>
2018-06-02 12:53:23 -0400

    add OptionsManager script, and adjust the alignment of the ui slider

---
+++ Assets/Scripts/OptionsManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OptionsManager : MonoBehaviour {
+
+    public Text playerCountLabel;
+    public int playerCount { get; set; }
+    public float playerCountF
+    {
+        set
+        {
+            playerCount = (int)value;
+            playerCountLabel.text = string.Format("Player Count: {0}", playerCount);
+        }
+    }
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Use this for initialization
+    void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+}
