91fb7a801951f3c27c70e01bbba0510e0b52a251
Stefan Salanski <stefan.salanski@gmail.com>
2022-02-28 22:57:32 -0500

    ensure a trick only accepts one card from each player (safety net)

--- Trick.gd
+++ Trick.gd
@@ -11,6 +11,7 @@ func _ready():
 	pass # Replace with function body.
 
 func accept_card(player, card):
+	assert(!cards.values().has(player))
 	card.set_faceup(true)
 	card.position = position.direction_to(player.position) * 50
 	card.look_at(player.position)
